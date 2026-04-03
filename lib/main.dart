858237184e4afc4fb5893f0d34249d62213298ab
鳥越幸樹 <niaoyuexingshu@gmail.com>
2026-04-03 17:28:32 +0900

    feat: firebaseの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_2026/app.dart';
+import 'package:flutter_app_2026/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
